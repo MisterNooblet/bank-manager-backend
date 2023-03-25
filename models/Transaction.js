2dbbd576668fd65c04caccf69eef1fae7c2cf28f
misternooblet <dragonroadsupport@gmail.com>
2023-03-26 02:10:53 +0300

    transaction to default null;

--- models/Transaction.js
+++ models/Transaction.js
@@ -10,7 +10,7 @@ const transactionSchema = new mongoose.Schema(
     to: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Account',
-      default: 'none'
+      default: null
     },
     amount: {
       type: Number,
