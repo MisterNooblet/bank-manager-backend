9e71b907b492ea9243949224dccceaaa4d9e15e2
misternooblet <dragonroadsupport@gmail.com>
2023-03-25 11:59:38 +0300

    can create user with credit pre defined

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,13 @@ import User from "../models/User.js";
 // @route   POST /api/v1/users
 // @access  Public
 export const createUser = asyncHandler(async (req, res, next) => {
-    const user = await User.create(req.body);
+    const { name, passportID, credit } = req.body
+    const user = await User.create({ name: name, passportID: passportID });
+    if (credit) {
+        const account = await Account.findById(user.accounts[0])
+        account.credit = credit
+        await account.save()
+    }
     res.status(200).json({
         success: true,
         data: user,
